e1ba9e7c17eb9aec03b1e55b99e3a3760a583b7d
knakna <kna@tut.by>
2023-11-26 23:02:36 +0300

    add 22-24 tasks

--- main.js
+++ main.js
@@ -81,3 +81,14 @@ let myVar1 = 11;
 
 myVar1--;
 
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+
+const sum = 10 + 10;
+
+
+const difference = 45 - 33;
